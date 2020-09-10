487938baaee1d75e267af7b011afef690529de60
Jo <66825013+Usaldudo@users.noreply.github.com>
2020-09-10 09:19:41 +0300

    UPD

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 /* GET home page. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello Git!' });
 });
